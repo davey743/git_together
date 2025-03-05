9afe2d4e056796b49e95aeb681946ba1cc87306f
davey743 <dh43529p@pace.edu>
2025-03-05 18:51:43 -0500

    reverse string

--- reverse-string/reverse_string.py
+++ reverse-string/reverse_string.py
@@ -1,5 +1,19 @@
 def reverse_string(str):
   ''' Reverse string str by modifying input array in-place 0(1) extra memomory '''
+  left, right = 0, len(str) - 1
+
+  while left < right:
+    str[left], str[right] = str[right], str[left]
+
+    left+= 1
+    right-= 1 
 
   return str
 
+str1 = ["h", "e", "l", "l", "o"]
+reverse_string(str1)
+print(str1)
+
+str2 = ["H", "a", "n", "n", "a", "h"]
+reverse_string(str2)
+print(str2)
